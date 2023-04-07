8013bfbbbd6256d21d656f607335a6597d9128dd
Aram <footyball97@gmail.com>
2023-04-08 01:54:08 +0400

    .

--- Classwork/Day 1/Two Sum.cpp
+++ Classwork/Day 1/Two Sum.cpp
@@ -1,18 +1,25 @@
 #include <iostream>
-#include <map>
+#include <unordered_map>
 #include <vector>
+
+std::vector<int> twoSum(std::vector<int>& nums, int target) {
+    std::unordered_map<int, int> hash_table;
+    for (int i = 0; i < nums.size(); i++) {
+        int difference = target - nums[i];
+        if (hash_table.count(difference)) {
+            return { hash_table[difference], i };
+        }
+        hash_table[nums[i]] = i;
+    }
+    return {};
+}
 
 int main() //wip
 {
-	std::vector<int> v{ 2, 7, 11, 15 };
-	int target;
-	std::cin >> target;
-	std::map<int, int> m;
-	int size = v.size();
-	for (int i = 0; i < size; ++i)
-		m.insert(target - v[i], i);
+    std::vector<int> v{ 4, 5, 6, 3, 8, 5, 0, 9 };
+    int target;
+    std::cin >> target;
+    std::cout << twoSum(v, target)[0];
 
-	
-	/*for (auto x : m)
-		std::cout << x.first << ' ' <<x.second << '\n';*/
+
 }
