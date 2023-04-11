0f34b02bbf8ea19f1c36311a127926272597bcda
NecroRAM <48534952+NecroRAM@users.noreply.github.com>
2023-04-11 04:00:41 +0400

    Update Two Sum.cpp

--- Classwork/Day 1/Two Sum.cpp
+++ Classwork/Day 1/Two Sum.cpp
@@ -16,10 +16,18 @@ std::vector<int> twoSum(std::vector<int>& nums, int target) {
 
 int main() //wip
 {
+	std::vector<int> v{ 2, 7, 11, 15 };
+	int target;
+	std::map<int, int> m;
+	std::cin >> target;
+	for (int i = 0; i < v.size(); ++i)
+	{
+		if (!m.count(target - v[i]))
+			m.insert(target - v[i], i);
+	}
+
     std::vector<int> v{ 4, 5, 6, 3, 8, 5, 0, 9 };
     int target;
     std::cin >> target;
     std::cout << twoSum(v, target)[0];
-
-
 }
