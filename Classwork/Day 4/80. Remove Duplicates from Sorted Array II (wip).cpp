3a0ef7bb995fa590336e3dace6ec4096409d8d80
Aram <footyball97@gmail.com>
2023-04-23 11:39:38 +0400

    homework 23.04

--- Classwork/Day 4/80. Remove Duplicates from Sorted Array II (wip).cpp
+++ Classwork/Day 4/80. Remove Duplicates from Sorted Array II (wip).cpp
@@ -4,9 +4,44 @@ public:
     {
         int count = 0;
         for (int i = 1; i < nums.size(); ++i)
-            if (nums[i] < nums[i - 1])
+            if (nums[i] > nums[i - 1])
                 ++count;
             else 
-            nums[i - count] = nums[i];
+				nums[i - count] = nums[i];
     }
 };
+
+
+// VS attempt
+
+// #include <iostream>
+// #include <vector>
+
+// int removeDuplicates(std::vector<int>& nums)
+// {
+    // int count = 0;
+    // if (nums.size() == 1)
+        // return 1;
+    // for (int i = 1; i < nums.size(); ++i)
+    // {
+        // while (i < nums.size() && nums[i] == nums[i - 1])
+        // {
+            // ++count;
+            // ++i;
+        // }
+        // nums[i - count + 1] = nums[i];
+        // i = i - count + 1;
+        // count = 0;
+    // }
+    // return count;
+// }
+
+// int main()
+ // {
+    // std::vector<int> nums{ 1,1,1,2,2,3 };
+    // std::cout << removeDuplicates(nums);
+    // std::cout << '\n';
+
+    // for (auto x : nums)
+        // std::cout << x << ' ';
+// }
