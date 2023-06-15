a20ecb592466e251754de1b40a7c3466189268b9
Aram <footyball97@gmail.com>
2023-06-15 17:15:02 +0400

    home

--- Homework/Data Structures/AVL Tree (wip).cpp
+++ Homework/Data Structures/AVL Tree (wip).cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <stack>
+#include <queue>
+#include <iterator>
 #include "D:\Downloads\Courses\Algs and Data Structs\PicsArt-Algs-and-DS\Homework\Data Structures\TreeNode.hpp"
 
 template <typename T>
@@ -24,7 +26,7 @@ public:
 			m_root = insert(m_root, val);
 		}
 	}
-
+	
 	// Methods
 	TreeNode<T>* min_element(TreeNode<T>* node) const
 	{
@@ -52,8 +54,7 @@ public:
 	}
 	TreeNode<T>* root() const
 	{
-		if (m_root)
-			return m_root;
+		return m_root;
 	}
 	void traverseRecursive(TreeNode<T>* node) const
 	{
@@ -62,6 +63,30 @@ public:
 		traverseRecursive(node->left);
 		print(node);
 		traverseRecursive(node->right);
+	}
+	void levelOrderTraversal() const
+	{
+		if (!m_root)
+			return;
+
+		TreeNode<T>* cur = m_root;
+		std::queue<TreeNode<T>*> q;
+		int max = 0;
+		q.push(cur); 
+
+		while (!q.empty())
+		{
+			max = cur->val;
+			cur = q.front();
+			if (max > cur->val)
+				std::cout << '\n';
+			if (cur->left)
+				q.push(cur->left);
+			if (cur->right)
+				q.push(cur->right);
+			print(cur);
+			q.pop();
+		}
 	}
 	void traverseRecursive() const
 	{
@@ -143,15 +168,15 @@ public:
 				node = node->left;
 		return (node ? node->val == target : false);
 	}
-	bool empty()
+	bool empty() const
 	{
 		return m_size > 0;
 	}
-	std::size_t size()
+	std::size_t size() const
 	{
 		return m_size;
 	}
-	int balance_factor(TreeNode<T>* node)
+	int balance_factor(TreeNode<T>* node) const
 	{
 		if (!node)
 			return 0;
@@ -176,7 +201,13 @@ public:
 		if (!node)
 		{
 			++m_size;
-			return new TreeNode<T>(p_val);
+			if (!m_root)
+			{
+				m_root = new TreeNode<T>(p_val);
+				return m_root;
+			}
+			else
+				return new TreeNode<T>(p_val);
 		}
 		else if (node->val < p_val)
 			node->right = insert(node->right, p_val);
@@ -205,7 +236,7 @@ public:
 	{
 		if (!node)
 			return node;
-		if (p_val < node->val)
+		if (p_val < node->val) 
 			node->left = remove(node->left, p_val);
 		else if (p_val > node->val)
 			node->right = remove(node->right, p_val);
@@ -225,18 +256,196 @@ public:
 				--m_size;
 				return tmp;
 			}
-			TreeNode<T>* tmp = min_element(node->right);
-			node->val = tmp->val;
-			node->right = remove(node->right, tmp->val);
+			else 
+			{
+				TreeNode<T>* tmp = min_element(node->right);
+				node->val = tmp->val;
+				node->right = remove(node->right, tmp->val);
+			}
+			int bf = balance_factor(node);
+			if (bf > 1 and balance_factor(node->left) >= 0)
+				return rightRotate(node);
+			if (bf > 1 and balance_factor(node->left) < 0)
+			{
+				node->left = leftRotate(node->left);
+				return rightRotate(node);
+			}
+			if (bf < -1 and balance_factor(node->right) <= 0)
+				return leftRotate(node);
+			if (bf < -1)
+			{
+				node->right = rightRotate(node->right);
+				return leftRotate(node);
+			}
 		}
-		return node;
 	}
+	void clear()
+	{
+		std::stack<TreeNode<T>*> s;
+		TreeNode<T>* node = m_root, * tmp = nullptr;
+		while (node or !s.empty())
+		{
+			while (node)
+			{
+				s.push(node);
+				node = node->left;
+			}
+			node = tmp = s.top();
+			s.pop();
+			node = node->right;
+			delete tmp;
+		}
+		m_root = nullptr;
+		m_size = 0;
+	}
+
+	//class Iterator
+	//{
+	//private:
+	//	TreeNode<T>* cur;
+	//public:
+	//	using iterator_category = std::bidirectional_iterator_tag;
+	//	using value_type = T;
+	//	using reference = T&;
+	//	using pointer = T*;
+	//	using difference_type = std::ptrdiff_t;
+
+	//	// Constructor and operator overloads
+
+	//	// Dereference operators
+	//	reference operator*() const;
+	//	pointer operator->() const;
+
+	//	// Increment and decrement operators
+	//	Iterator& operator++();
+	//	Iterator operator++(int);
+	//	Iterator& operator--();
+	//	Iterator operator--(int);
+
+	//	// Equality and inequality operators
+	//	bool operator==(const Iterator& other) const;
+	//	bool operator!=(const Iterator& other) const;
+	//};
+
+	//// Concept for a valid AVL iterator
+	//template <typename Iterator>
+	//concept ValidAVLIterator = std::input_or_output_iterator<Iterator> &&
+	//	std::convertible_to<typename Iterator::value_type, T>;
+
+	//// Public member functions
+
+	//Iterator begin();
+	//Iterator end();
+
+	//// Additional member functions and definitions
 };
+
+// Iterator member function definitions
+
+//template <typename T>
+//typename AVL<T>::Iterator AVL<T>::begin()
+//{
+//	return min_element(m_root);
+//}
+//
+//template <typename T>
+//typename AVL<T>::Iterator AVL<T>::end()
+//{
+//	return max_element(m_root);
+//}
+//
+//template <typename T>
+//typename AVL<T>::Iterator::reference AVL<T>::Iterator::operator*() const
+//{
+//	return this->cur->val;
+//}
+//
+//template <typename T>
+//typename AVL<T>::Iterator::pointer AVL<T>::Iterator::operator->() const
+//{
+//	return this->cur;
+//}
+//
+//template <typename T> //WIP!!!!!!
+//typename AVL<T>::Iterator& AVL<T>::Iterator::operator++()
+//{
+//	// Increment the iterator to the next element
+//}
+//
+//template <typename T>
+//typename AVL<T>::Iterator AVL<T>::Iterator::operator++(int)
+//{
+//	// Post-increment the iterator to the next element
+//}
+//
+//template <typename T>
+//typename AVL<T>::Iterator& AVL<T>::Iterator::operator--()
+//{
+//	// Decrement the iterator to the previous element
+//}
+//
+//template <typename T>
+//typename AVL<T>::Iterator AVL<T>::Iterator::operator--(int)
+//{
+//	// Post-decrement the iterator to the previous element
+//}
+//
+//template <typename T>
+//bool AVL<T>::Iterator::operator==(const Iterator& other) const
+//{
+//	// Compare if two iterators are equal
+//}
+//
+//template <typename T>
+//bool AVL<T>::Iterator::operator!=(const Iterator& other) const
+//{
+//	// Compare if two iterators are not equal
+//}
+
 
 int main() 
 {
 	AVL<int> avl{ 2, 1, 4, 5, 3, 10, 9, 9 };
+
+	std::cout << '\n';
+	std::cout << "level1:\n";
+	avl.levelOrderTraversal();
+
+	std::cout << '\n';
+	std::cout << "rec:\n";
 	avl.traverseRecursive();
 	std::cout << '\n';
+
+	std::cout << "iter:\n";
 	avl.traverseIterative();
+	std::cout << '\n';
+
+	avl.remove(avl.root(), 4);
+	std::cout << avl.size();
+	std::cout << '\n';
+
+	avl.remove(avl.root(), avl.min_element(avl.root()->right)->val);
+	std::cout << avl.size();
+	std::cout << '\n';
+
+	std::cout << "iter:\n";
+	avl.traverseIterative();
+	avl.clear();
+	std::cout << '\n';
+
+	std::cout << "\nlevel2:\n";
+	avl.levelOrderTraversal();
+	avl.insert(avl.root(), 5);
+	std::cout << avl.root()->val << '\n';
+
+	for (int i = 10; i < 16; ++i)
+		avl.insert(avl.root(), i);
+
+	std::cout << '\n';
+	std::cout << "level3:\n";
+	avl.levelOrderTraversal();
+	std::cout << '\n';
+	std::cout << "iter:\n";
+	avl.traverseIterative();
+
 }
